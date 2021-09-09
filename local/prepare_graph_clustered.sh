aef086891e7fe761c6eac0edc89e1641f91b090c
Aku Rouhe <aku.rouhe@aalto.fi>
2021-09-09 12:57:47 +0300

    Updated graph prep

--- local/prepare_graph_clustered.sh
+++ local/prepare_graph_clustered.sh
@@ -6,17 +6,18 @@
 
 set -e -o pipefail
 
-stage=0
-train_set=train_clean_5
-valid_set=dev_clean_2
-rootdir=data
+dataroot=kaldi-s5/data
+dataroot=kaldi-s5/data
+train_set=train
+valid_set=parl-dev-all-fixed
+alipath=kaldi-s5/exp/i/tri4j_ali
 
-nj=10
-
-tree_dir=exp/chain/tree
+tree_dir=kaldi-s5/exp/chain/tree
+lang=kaldi-s5/data/lang_chain
+test_lang=kaldi-s5/data/lang_test_varikn.bpe1750.d0.0001/
 graph=exp/chain/graph
-langdir=data/lang
-lang=data/lang/lang_chain
+
+stage=0
 
 . ./path.sh
 . ./cmd.sh
@@ -28,8 +29,8 @@ if [ $stage -le 0 ]; then
   echo "Estimating a phone language model for the denominator graph..."
   mkdir -p $graph/log
   $train_cmd $graph/log/make_phone_lm.log \
-             cat $rootdir/$train_set/text \| \
-             steps/nnet3/chain/e2e/text_to_phones.py --between-silprob 0.1 \
+             cat $dataroot/$train_set/text \| \
+             local/text_to_phones.py --between-silprob 0.1 \
              $lang \| \
              utils/sym2int.pl -f 2- $lang/phones.txt \| \
              chain-est-phone-lm --num-extra-lm-states=2000 \
@@ -45,9 +46,9 @@ if [ $stage -le 1 ]; then
   cp $tree_dir/tree $graph/tree
   echo "Making denominator graph..."
   $train_cmd $graph/log/make_den_fst.log \
-	     chain-make-den-fst $graph/tree $graph/final.mdl \
-	     $graph/phone_lm.fst \
-	     $graph/den.fst $graph/normalization.fst
+       chain-make-den-fst $graph/tree $graph/final.mdl \
+       $graph/phone_lm.fst \
+       $graph/den.fst $graph/normalization.fst
 fi
 
 
@@ -56,25 +57,25 @@ if [ $stage -le 2 ]; then
   lex=$lang/L.fst
   oov_sym=`cat $lang/oov.int` || exit 1;
   for x in $train_set $valid_set; do
-    sdata=$rootdir/$x/split$nj;
-    [[ -d $sdata && $rootdir/$x/feats.scp -ot $sdata ]] || split_data.sh $rootdir/$x $nj || exit 1;
+    nj=$(cat "$alipath"_$x/num_jobs)
+    sdata=$dataroot/$x/split$nj;
+    [[ -d $sdata && $dataroot/$x/feats.scp -ot $sdata ]] || split_data.sh $dataroot/$x $nj || exit 1;
     $train_cmd JOB=1:$nj $graph/$x/log/compile_graphs.JOB.log \
-    	       compile-train-graphs $scale_opts --read-disambig-syms=$lang/phones/disambig.int \
-    	       $graph/tree $graph/final.mdl $lex \
-    	       "ark:sym2int.pl --map-oov $oov_sym -f 2- $lang/words.txt < $sdata/JOB/text|" \
-    	       "ark,scp:$graph/$x/fst.JOB.ark,$graph/$x/fst.JOB.scp" || exit 1;
+             compile-train-graphs $scale_opts --read-disambig-syms=$lang/phones/disambig.int \
+             $graph/tree $graph/final.mdl $lex \
+             "ark:sym2int.pl --map-oov $oov_sym -f 2- $lang/words.txt < $sdata/JOB/text|" \
+             "ark,scp:$graph/$x/fst.JOB.ark,$graph/$x/fst.JOB.scp" || exit 1;
     $train_cmd JOB=1:$nj $graph/$x/log/make_num_fst.JOB.log \
-    	       chain-make-num-fst-e2e $graph/final.mdl $graph/normalization.fst \
-    	       scp:$graph/$x/fst.JOB.scp ark,scp:$graph/$x/num.JOB.ark,$graph/$x/num.JOB.scp
-    for id in $(seq $nj); do cat $graph/$x/num.$id.scp; done > $graph/$x/num.scp
+             chain-make-num-fst-e2e $graph/final.mdl $graph/normalization.fst \
+             scp:$graph/$x/fst.JOB.scp ark,scp:$graph/$x/num.JOB.ark,$graph/$x/num.JOB.scp
   done
 fi
 
 if [ $stage -le 3 ]; then
   echo "Making HCLG full graph..."
   utils/lang/check_phones_compatible.sh \
-    $langdir/lang_nosp_test_tgsmall/phones.txt $lang/phones.txt
+    $test_lang/phones.txt $lang/phones.txt
   utils/mkgraph.sh \
-    --self-loop-scale 1.0 $langdir/lang_nosp_test_tgsmall \
-    $graph $graph/graph_tgsmall || exit 1;
+    --self-loop-scale 1.0 $test_lang \
+    $graph $graph/graph_test || exit 1;
 fi
